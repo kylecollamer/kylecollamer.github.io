1664e616b5575f45d600004b8e7d91ba02ad5077
Kyle Collamer <kylecollamer@gmail.com>
2025-06-15 23:17:22 -0400

    fixed typo

--- scripts/watkinsglen-slideshow-1.js
+++ scripts/watkinsglen-slideshow-1.js
@@ -1,7 +1,7 @@
 let imagesWatkinsGlen = [];
 let slideIndexWatkinsGlen = 0;
 
-fetch('motorsport-images/watkins-glen-imsa-2024-images/images.json')
+fetch('motorsport-images/watkins-glen-imsa-2024-images/track-pictures/images.json')
   .then(response => response.json())
   .then(data => {
     imagesWatkinsGlen = data;
@@ -13,7 +13,7 @@ function showSlidesWatkinsGlen() {
   const imgElement = document.getElementById('slideshow-image-watkinsglen');
   const captionElement = document.getElementById('caption-watkinsglen');
   const current = imagesWatkinsGlen[slideIndexWatkinsGlen];
-  imgElement.src = `motorsport-images/watkins-glen-imsa-2024-images/${current.filename}`;
+  imgElement.src = `motorsport-images/watkins-glen-imsa-2024-images/track-pictures/${current.filename}`;
   captionElement.textContent = current.caption || '';
   slideIndexWatkinsGlen = (slideIndexWatkinsGlen + 1) % imagesWatkinsGlen.length;
   setTimeout(showSlidesWatkinsGlen, 4000);
