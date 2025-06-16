0eb2b0dc0278e420ed64eca5e929a16c391ae47c
Kyle Collamer <kylecollamer@gmail.com>
2025-06-15 23:12:45 -0400

    changed front page of motorsports.

---
+++ scripts/watkinsglen-slideshow.js
@@ -0,0 +1,20 @@
+let imagesWatkinsGlen = [];
+let slideIndexWatkinsGlen = 0;
+
+fetch('motorsport-images/watkins-glen-imsa-2024-images/images.json')
+  .then(response => response.json())
+  .then(data => {
+    imagesWatkinsGlen = data;
+    showSlidesWatkinsGlen();
+  });
+
+function showSlidesWatkinsGlen() {
+  if (imagesWatkinsGlen.length === 0) return;
+  const imgElement = document.getElementById('slideshow-image-watkinsglen');
+  const captionElement = document.getElementById('caption-watkinsglen');
+  const current = imagesWatkinsGlen[slideIndexWatkinsGlen];
+  imgElement.src = `motorsport-images/watkins-glen-imsa-2024-images/${current.filename}`;
+  captionElement.textContent = current.caption || '';
+  slideIndexWatkinsGlen = (slideIndexWatkinsGlen + 1) % imagesWatkinsGlen.length;
+  setTimeout(showSlidesWatkinsGlen, 4000);
+}
